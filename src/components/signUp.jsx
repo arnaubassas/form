89befeb178504d6d8bd75cb1c99ae0871390c44b
arnaubassas <arnau_b17@hotmail.com>
2023-02-14 13:25:04 +0100

    upgrade confirm same mail and password

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -2,25 +2,37 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, formState} from "react-hook-form";
+
+
 
 const SignUp = () => {
-    const { control, handleSubmit } = useForm({
+    
+
+    const { control, handleSubmit, formState } = useForm({
         defaultValues: {
             firstName: '',
             surNames: '',
             userName: '',
             place: '',
             passWord: '',
-            repeatPassWord: '',
+            confirmPassWord: '',
             mail: '',
             repeatMail: ''
         }
     });
+
+    console.log(formState.errors)
+    
     const onSubmit = (data: any) => console.log(data);
 
     return (
         <div>
+            <div>
+                <ul>
+                    {formState.errors.repeatMail?.message && <li>{formState.errors.repeatMail?.message}</li>}
+                </ul>
+            </div>
             <div style={{ textAlign: 'center', margin: "30px" }}>
                 <Typography variant="h4" gutterBottom >
                     Sign Up:
@@ -31,13 +43,13 @@ const SignUp = () => {
                     <Controller
                         name="firstName"
                         control={control}
-                        render={({ field }) => <TextField {...field}
+                        render={({ field }) => <TextField {...field} 
                             required
                             type="text"
                             label="Name"
                         />}
                     />
-                    <span style={{  margin: "10px" }}/>
+                    <span style={{ margin: "10px" }} />
                     <Controller
                         name="surNames"
                         control={control}
@@ -59,13 +71,13 @@ const SignUp = () => {
                             variant="filled"
                         />}
                     />
-                    <span style={{  margin: "10px" }}/>
+                    <span style={{ margin: "10px" }} />
                     <Controller
                         name="place"
                         control={control}
                         render={({ field }) => <TextField {...field}
                             required
-                            label="Where you live"
+                            label="City"
                             variant="filled"
                             type="text"
                         />}
@@ -75,20 +87,24 @@ const SignUp = () => {
                     <Controller
                         name="passWord"
                         control={control}
-                        render={({ field }) => <TextField {...field}
+                        render={({ field }) => <TextField {...field} 
                             required
                             label="Password"
                             type="password"
                         />}
                     />
-                    <span style={{  margin: "10px" }}/>
+                    <span style={{ margin: "10px" }} />
                     <Controller
-                        name="repeatPassWord"
+                        name="confirmPassWord"
                         control={control}
-                        render={({ field }) => <TextField {...field}
+                        rules={{validate: (value, values) => 
+                            values.passWord === values.confirmPassWord || 'Use the same password'
+                        }}
+                        render={({ field}) => <TextField {...field}
                             required
                             type="password"
                             label="Repeat Password"
+                             helperText={formState.errors.confirmPassWord?.message}
                         />}
                     />
                 </div>
@@ -103,15 +119,19 @@ const SignUp = () => {
                             variant="filled"
                         />}
                     />
-                    <span style={{  margin: "10px" }}/>
+                    <span style={{ margin: "10px" }} />
                     <Controller
                         name="repeatMail"
                         control={control}
+                        rules={{validate: (value, values) => 
+                            values.mail === values.repeatMail || 'Use the same mail'
+                        }}
                         render={({ field }) => <TextField {...field}
                             required
                             label="Repeat E-Mail"
                             type="email"
                             variant="filled"
+                            helperText={formState.errors.repeatMail?.message}
                         />}
                     />
                 </div>
